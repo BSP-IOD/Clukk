f939ba239b72521a8c4f4b7dcdbd09e8a37c9427
BSP-IOD <innovativeopsdiv.bsp@protonmail.com>
2026-03-19 19:16:00 +0100

    Create hour2second.py

---
+++ hour2second.py
@@ -0,0 +1,17 @@
+print("\n" * 100)
+print("""
+   ______   __            __      __
+  / ____/  / /  __  __   / /__   / /__
+ / /      / /  / / / /  / //_/  / //_/
+/ /___   / /  / /_/ /  /  <    /  <
+\____/  /_/   \____/  /_/|_|  /_/|_|
+
+             Release V2.1
+             
+             Hour2Second
+""")
+hours = float(input("Enter amount of hours:"))
+
+seconds = hours * 60 * 60
+
+print("Hours converted to seconds:", seconds)
