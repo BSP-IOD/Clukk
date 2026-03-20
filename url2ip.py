b601cc0f73b225cbf1587606f65306606511fb3b
BSP/IOD <innovativeopsdiv.bsp@protonmail.com>
2026-03-20 21:22:39 +0100

    Update url2ip.py

--- url2ip.py
+++ url2ip.py
@@ -1,21 +1,13 @@
 import socket
 
-print("\n" * 100)
 print("""
-   ______   __            __      __
-  / ____/  / /  __  __   / /__   / /__
- / /      / /  / / / /  / //_/  / //_/
-/ /___   / /  / /_/ /  /  <    /  <
-\____/  /_/   \____/  /_/|_|  /_/|_|
-
-             Release V2.1
+                Clukk
+             Release V2.3
              
                 URL2IP
 """)
-
+print("")
 ip = " "
-
 ip = input("Enter URL: ")
-
 x = socket.gethostbyname(ip)
 print("IP: ", x)
