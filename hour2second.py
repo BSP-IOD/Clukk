49747750ec1686c2115ac8678e6a1613dbd2540b
BSP/IOD <innovativeopsdiv.bsp@protonmail.com>
2026-03-20 21:21:38 +0100

    Update hour2second.py

--- hour2second.py
+++ hour2second.py
@@ -1,17 +1,10 @@
-print("\n" * 100)
 print("""
-   ______   __            __      __
-  / ____/  / /  __  __   / /__   / /__
- / /      / /  / / / /  / //_/  / //_/
-/ /___   / /  / /_/ /  /  <    /  <
-\____/  /_/   \____/  /_/|_|  /_/|_|
-
-             Release V2.1
+                Clukk
+             Release V2.3
              
              Hour2Second
 """)
+print("")
 hours = float(input("Enter amount of hours:"))
-
 seconds = hours * 60 * 60
-
 print("Hours converted to seconds:", seconds)
